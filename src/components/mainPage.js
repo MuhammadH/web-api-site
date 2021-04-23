957f5751a752e47c6f031407e30154187598d73f
MuhammadH <Muhammadhauris@gmail.com>
2021-04-22 22:30:19 -0600

    site code

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -8,7 +8,55 @@ class MainPage extends Component {
 
     render() {
 
+        async function fetchCur() {
+            let user_cur = '';
+            await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=47a11b840b6e4f1b951c65025ce182bd`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    user_cur = data.currency.code;
+                    console.log(user_cur + ' li');
+                })
+            console.log(user_cur + ' lo');
+            return user_cur;
+        }
+        async function conversionAPI(symb, amount) {
+            let new_amount = 0.1;
+
+            let inputs = {
+                input_data:{"amount": amount, "currency": symb}
+            };
+
+            console.log(symb + '_' + amount);
+            await fetch('https://conversion-api-hussain.herokuapp.com/conversion', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(inputs.input_data)
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log (data);
+                    new_amount = data.new_amount;
+                    console.log (new_amount + ' li');
+                })
+            console.log (new_amount + ' lo');
+            return new_amount;
+        }
+        fetchCur().then(result => {
+            console.log(result + ' cur result');
+            return result;
+        }).then(res => {
+            conversionAPI(res, 25.0).then(result => {
+                console.log(result + ' should be around 30.09');
+            })
+        })
+
         let imgUrl = 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Variegated_golden_frog_%28Mantella_baroni%29_Ranomafana.jpg';
+
+        let euro_price = 25.0;
 
         return (
             <Card>
