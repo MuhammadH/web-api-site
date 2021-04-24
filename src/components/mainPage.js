cb1d73fbbd3a160714ffa862324dff437b09882f
MuhammadH <Muhammadhauris@gmail.com>
2021-04-23 20:54:56 -0600

    site code

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -31,60 +31,6 @@ class MainPage extends Component {
         this.getAPIData()
     }
 
-    getAPIData() {
-        async function fetchCur() {
-            let user_cur = '';
-            await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=fed45de65e7d4971897a53add8c7250a`)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                    user_cur = data.currency.code;
-                    console.log(user_cur + ' li');
-                })
-            console.log(user_cur + ' lo');
-            return user_cur;
-        }
-        async function conversionAPI(symb, amount) {
-            let new_amount = 0.1;
-
-            let inputs = {
-                input_data:{"amount": amount, "currency": symb}
-            };
-
-            console.log(symb + '_' + amount);
-            await fetch('https://conversion-api-hussain.herokuapp.com/conversion', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(inputs.input_data)
-            })
-                .then(response => response.json())
-                .then(data => {
-                    console.log (data);
-                    new_amount = data.new_amount;
-                    console.log (new_amount + ' li');
-                })
-            console.log (new_amount + ' lo');
-            return new_amount;
-        }
-        fetchCur().then(result => {
-            console.log(result + ' cur result');
-            this.setState({
-                region_cur_code: result
-            });
-            return result;
-        }).then(res => {
-            conversionAPI(res, 25.0).then(result => {
-                console.log(result + ' should be around 30.09');
-                this.setState({
-                    region_amount: result
-                });
-            })
-        })
-    }
-
     updateEvent(event){
         let updateEvent = Object.assign({}, this.state.purchaseData);
 
@@ -164,6 +110,60 @@ class MainPage extends Component {
                     address: ''
                 }
             });
+        })
+    }
+
+    getAPIData() {
+        async function fetchCur() {
+            let user_cur = '';
+            await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=fed45de65e7d4971897a53add8c7250a`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    user_cur = data.currency.code;
+                    console.log(user_cur + ' li');
+                })
+            console.log(user_cur + ' lo');
+            return user_cur;
+        }
+        async function conversionAPI(symb, amount) {
+            let new_amount = 0.1;
+
+            let inputs = {
+                input_data:{"amount": amount, "currency": symb}
+            };
+
+            console.log(symb + '_' + amount);
+            await fetch('https://conversion-api-hussain.herokuapp.com/conversion', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(inputs.input_data)
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log (data);
+                    new_amount = data.new_amount;
+                    console.log (new_amount + ' li');
+                })
+            console.log (new_amount + ' lo');
+            return new_amount;
+        }
+        fetchCur().then(result => {
+            console.log(result + ' cur result');
+            this.setState({
+                region_cur_code: result
+            });
+            return result;
+        }).then(res => {
+            conversionAPI(res, 25.0).then(result => {
+                console.log(result + ' should be around 30.09');
+                this.setState({
+                    region_amount: result
+                });
+            })
         })
     }
 
